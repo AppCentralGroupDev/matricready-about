f7d2f34af6af7ac4f04d501dccb10feaef4d6c24
Bamgbose Aderounmu <aderounmubamgbose@yahoo.com>
2023-01-17 11:11:26 +0100

    Website added

--- README.md
+++
@@ -1,1 +0,0 @@
-# matricready-about
---
+++ index.js
@@ -0,0 +1,4 @@
+const all_rights_reserved = document.getElementById('a-r-l')
+all_rights_reserved.innerHTML = `
+© ${new Date().getFullYear()} <a href="#" class="hover:underline">Matricready™</a>. All Rights Reserved.
+`
